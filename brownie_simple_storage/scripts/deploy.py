758406df402f5928d74c69d102626198ce3d982b
jissac <jaeeysuk@gmail.com>
2022-04-14 18:23:32 -0500

    brownie contract

---
+++ brownie_simple_storage/scripts/deploy.py
@@ -0,0 +1,28 @@
+from brownie import accounts, config, SimpleStorage
+import os
+
+def deploy_simple_storage():
+    # load account 3 ways, 
+    # 1. test account 2. eth account thru cli 3. using venv
+
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+    print(simple_storage)
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(15, {f"from": account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
+
+    # account = accounts.load("smart-contract-account")
+    # print(account)
+
+    # account = accounts.add(os.getenv("PRIVATE_KEY"))
+    # account = accounts.add(config["wallet"]["from_key"]) # more secure version of above
+    # print(account)
+
+
+def main():
+    deploy_simple_storage()
+    print("Hello from brownie")
