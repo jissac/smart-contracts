e58993e465bb148dd04a485034f6d8cbfc4d9fc8
jissac <jaeeysuk@gmail.com>
2022-04-27 17:07:33 -0500

    simple_storage in brownie

--- brownie_simple_storage/tests/test_simple_storage.py
+++ brownie_simple_storage/tests/test_simple_storage.py
@@ -1,1 +1,27 @@
-from brownie import
+from brownie import SimpleStorage, accounts
+
+def test_deploy():
+    # arrange, act, assert
+
+    # arrange
+    account = accounts[0]
+
+    # act
+    simple_storage = SimpleStorage.deploy({"from": account})
+    starting_value = simple_storage.retrieve()
+    expected = 0
+
+    # assert
+    assert starting_value == expected
+
+def test_updating_storage():
+    # arrange
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+
+    # act
+    expected = 15
+    simple_storage.store(15, {f"from": account})
+
+    # assert
+    assert expected == simple_storage.retrieve()
